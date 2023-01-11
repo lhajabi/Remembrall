1df3a33c96bf3cc2b4b4f6297320b83c923bb3fc
lhajabi <lhajabi@gmail.com>
2023-01-11 17:40:51 +0300

    5:50pm 11/1/2023

--- app/src/main/java/com/example/remembrall/MapFragment.kt
+++ app/src/main/java/com/example/remembrall/MapFragment.kt
@@ -1,8 +1,10 @@
 package com.example.remembrall
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,25 +12,37 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment(R.layout.fragment_map){//}, OnMapReadyCallback {
-    //private lateinit var mMap: GoogleMap
-    private lateinit var mapFragment : SupportMapFragment
+class MapsFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val PSUT = LatLng(32.023328, 35.876253)
+        googleMap.clear()
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(PSUT))
+        googleMap.addMarker(MarkerOptions().position(PSUT).title("Marker in PSUT"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(PSUT))
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        //mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        //mapFragment.getMapAsync(this)
-
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
-/*
-     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(
-            MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-*/
 }
--- app/src/main/java/com/example/remembrall/SecondActivity.kt
+++ app/src/main/java/com/example/remembrall/SecondActivity.kt
@@ -1,8 +1,8 @@
 package com.example.remembrall
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,19 +13,25 @@ import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.gms.tasks.OnTokenCanceledListener
 
 class SecondActivity : AppCompatActivity() {
-    private var latLong : String = "NoWhere"
-    @SuppressLint("MissingInflatedId")
+    public var latLong : String = "32.023328,35.876253"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         val txtLoc:TextView = findViewById(R.id.TxtVLocation)
+        val btnShow:Button = findViewById(R.id.button2)
+        val btnSave:Button = findViewById(R.id.button3)
         getLocation()
         txtLoc.text=latLong
+        btnShow.setOnClickListener(){
+            setContentView(R.layout.fragment_map)
+        }
+        btnSave.setOnClickListener(){
+            //add txtLoc.text to data base
+        }
 
     }
 
-    @SuppressLint("MissingPermission")
-    private fun getLocation(){
+    public fun getLocation(){
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
             ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
@@ -36,25 +42,19 @@ class SecondActivity : AppCompatActivity() {
             override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
             override fun isCancellationRequested() = false })
             .addOnSuccessListener { location: Location? ->
-                if (location == null)
-                    latLong = "Null"
-                else {
-                    latLong = location.latitude.toString()+ location.longitude.toString()
-                }
-                Toast.makeText(
-                    applicationContext,
-                    latLong,
-                    Toast.LENGTH_SHORT
-                ).show()
+                latLong = if (location == null) "NULL"
+                else location.latitude.toString()+ location.longitude.toString()
 
+               // Toast.makeText(this, "success "+latLong, Toast.LENGTH_SHORT).show()
             }
         loc.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
             override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
             override fun isCancellationRequested() = false })
             .addOnFailureListener(){
                 latLong = "FAIL"
+                Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show()
             }
-
+        Toast.makeText(this, latLong, Toast.LENGTH_SHORT).show()
         return
     }
 
